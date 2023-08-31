16e4aa44045f8b680c5b09b229bc7eca7caccbdd
mactkg <mactkg@gmail.com>
2023-08-31 21:15:14 +0900

    v0.1.0

--- main.ts
+++ main.ts
@@ -60,7 +60,7 @@ const channel = new Command()
 const cli = new Command()
   .name("slack-icon-downloader")
   .description("Slack icon downloader")
-  .version("v0.0.1")
+  .version("v0.1.0")
   .command("group", group)
   .command("channel", channel);
 await cli.parse();
